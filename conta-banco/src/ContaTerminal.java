42a8d0aed9104677a25fe9695294511b8df46a52
Mateus Soares <mateus.s18@hotmail.com>
2023-09-11 12:54:38 -0300

    nova atualização projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -6,17 +6,17 @@ public class ContaTerminal {
 
         Scanner entrada = new Scanner(System.in);
         System.out.println("Por favor, digite o usuário: ");
-        int numero = entrada.nextInt();
+        int numeroUsuario = entrada.nextInt();
         entrada.nextLine();
         System.out.println("Por favor, digite o número da agência: ");
-        String agencia = entrada.nextLine();
+        String numeroAgencia = entrada.nextLine();
         System.out.println("Por favor, digite o nome do cliente");
         String nomeCliente = entrada.nextLine();
         System.out.println("Por favor, digite o valor que deseja depositar");
-        int saldo = entrada.nextInt();
+        int valorSaldo = entrada.nextInt();
 
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
-             ", conta " + numero + " e seu saldo R$" + saldo + ",00 já está disponível para saque.");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia +
+             ", conta " + numeroUsuario + " e seu saldo R$" + valorSaldo + ",00 já está disponível para saque.");
 
         entrada.close();
     }
